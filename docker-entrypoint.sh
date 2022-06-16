05e8b68a278adf4a15eadf3a85bab4b106e62858
PMJ <pmj@pmj.hu>
2022-06-16 13:59:26 +0200

    Create backup of rclone config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,17 @@ if test -f /rclone.conf; then
     # AND $UPLOAD ENV VAR SPECIFIED
     if test ! -z "$UPLOAD"; then
 
+        # MAKE A COPY OF THE CONFIG
+        cp /rclone.conf /r.conf
+
         # UPLOAD DOWNLOADED FILES IN CURRENT DIRECTORY
-        rclone --config /rclone.conf copy --progress . $UPLOAD
+        rclone --config /r.conf copy --progress . $UPLOAD
+
+        # IF THE CONFIG COPY WAS UPDATED
+        if ! diff -q /r.conf /rclone.conf > /dev/null; then
+
+            # UPDATE THE ORIGINAL CONFIG
+            cat /r.conf > /rclone.conf
+        fi
     fi
 fi
